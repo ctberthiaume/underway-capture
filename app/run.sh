82d2a0e62d04f7ec19643bc72d0b76adc70933d8
Chris Berthiaume <chrisbee@uw.edu>
2019-04-04 22:03:34 -0700

    Put data in the right place

--- app/run.sh
+++ app/run.sh
@@ -9,4 +9,4 @@ echo "Starting UDP capture in /mnt/underway" >&2
 # All writes are appends, so restarting this script
 # or container will not erase existing data.
 #/app/udplisten.py | tee -a KM_feed.txt | /app/kmfeedsplit
-/app/udplisten-f04c050 -p 1234 -f KM_feed.txt -b 2048
+/app/udplisten-f04c050 -p 1234 -f /mnt/underway/KM_feed.txt -b 2048
