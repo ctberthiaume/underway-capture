efa853755aacf77a65b9bc9d669b438b5930663e
Chris Berthiaume <chrisbee@uw.edu>
2019-04-10 22:38:39 -0700

    Don't log to stderr

--- app/run.sh
+++ app/run.sh
@@ -14,7 +14,7 @@ echo "Starting UDP capture on port $port appending to directory $outdir" >&2
 # All writes are appends, so restarting this script
 # or container will not erase existing data.
 # Switch to ./udplisten.linux-amd64.f04c050 for linux
-./udplisten.darwin.f04c050 -p "$port" -b 2048 | \
+./udplisten.darwin.f04c050 -q -p "$port" -b 2048 | \
     tee -a "$outdir"/KM_feed.txt | 
     ./nulleraser | \  # remove null bytes if present
     ./kmfeedsplit "$outdir"
